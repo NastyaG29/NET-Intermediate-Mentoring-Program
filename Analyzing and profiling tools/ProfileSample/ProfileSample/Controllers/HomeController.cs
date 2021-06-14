3c621358594a1ab783893ab934baec86975981d7
Anastasiya Horyna <Anastasiya_Horyna@epam.com>
2021-06-15 01:14:56 +0300

    Add analyzing and profiling tools.

---
+++ Analyzing and profiling tools/ProfileSample/ProfileSample/Controllers/HomeController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ProfileSample.DAL;
+using ProfileSample.Models;
+
+namespace ProfileSample.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            var imageModels = new List<ImageModel>();
+
+            using (var context = new ProfileSampleEntities()) 
+                imageModels = context.ImgSources.Take(20).Select(s => new ImageModel {Name = s.Name, Url = s.Url})
+                    .ToList();
+
+            return View(imageModels);
+        }
+
+        public ActionResult Convert()
+        {
+            var files = Directory.GetFiles(Server.MapPath("~/Content/Img"), "*.jpg");
+
+            using (var context = new ProfileSampleEntities())
+            {
+                foreach (var file in files)
+                {
+                    var imageSource = new ImgSource()
+                    {
+                        Name = Path.GetFileName(file),
+                        Url = file
+                    };
+
+                    context.ImgSources.Add(imageSource);
+                    context.SaveChanges();
+                } 
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ Analyzing and profiling tools/ProfileSample/ProfileSample/Models/ImageModel.cs
@@ -0,0 +1,11 @@
+﻿namespace ProfileSample.Models
+{
+    public class ImageModel
+    {
+        public string Name { get; set; }
+
+        //public byte[] Data { get; set; } optimized
+
+        public string Url { get; set; }
+    }
+}
